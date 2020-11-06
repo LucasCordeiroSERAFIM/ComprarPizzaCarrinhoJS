673cae496a798a5608a39605d25573264aabae4f
LucasCordeiroSERAFIM <42965466+LucasCordeiroSERAFIM@users.noreply.github.com>
2020-11-06 09:09:58 -0300

    Add files via upload
    Carrinho de Compras com JS

---
+++ script.js
@@ -0,0 +1,192 @@
+let cart = [];
+let modalQt = 1;
+let modalKey = 0;
+
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+
+// Listagem das pizzas
+pizzaJson.map((item, index) => {
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.setAttribute('data-key', index);
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+
+    /*Modal*/
+    /*zera a tag a  eadd evento de click nela */
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log("Cliclou na  pizza");
+        /**envia dados pro modal atraves do index de cada item  iniciando a variavel quantidade como 1*/
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+        modalKey = key;
+
+        /**preenchendo dados no modal  atravez da key */
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        /*selecionar item pequeno medio ou grande*/
+        c('.pizzaInfo--size.selected').classList.remove('selected'); /**disceleciona */
+        //tamanho para poder clicar em outros
+
+        /*percorre todos os tamanhos atravez do codigo delas */
+        cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            //se o codigo do tamanho for 2 seleciona(deixa azul)
+            if (sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+        /*Abrir modal passando a qtd inicial*/
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+    });
+
+    c('.pizza-area').append(pizzaItem);
+});
+
+// Eventos do MODAL
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
+/**para cada botao q eu quero q fexe o modal* */
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal);
+});
+
+/*botões mais e menos  do modal */
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+/*Selecionar tamanho percorrendo o array de tamanhos e escolhendo os tamanhaos*/
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
+
+/*Carrinho*/
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+    let identifier = pizzaJson[modalKey].id + '@' + size;
+    let key = cart.findIndex((item) => item.identifier == identifier);
+    // qual pizza 
+    console.log("Pizza" + modalKey);
+    //tamanho travz da key de cada tamanho no html
+    let tamanho = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+    console.log(" Tamanho Pizza" + tamanho);
+
+    if (key > -1) {
+        cart[key].qt += modalQt;
+    } else {
+        cart.push({
+            identifier,
+            id: pizzaJson[modalKey].id,
+            size,
+            qt: modalQt
+        });
+    }
+    updateCart();
+    closeModal();
+});
+
+
+c('.menu-openner').addEventListener('click', () => {
+    if (cart.length > 0) {
+        c('aside').style.left = '0';
+    }
+});
+c('.menu-closer').addEventListener('click', () => {
+    c('aside').style.left = '100vw';
+});
+
+/**atualizar carrinho mastra ele ou não  e calcular valor*/
+function updateCart() {
+    c('.menu-openner span').innerHTML = cart.length;
+
+    if (cart.length > 0) {
+        c('aside').classList.add('show');
+        c('.cart').innerHTML = '';
+
+        /*matemática do carrinho itens iniciando em 0*/
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
+        for (let i in cart) {
+            let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+            /**subtotal do carrinho quandiade * preço  */
+            subtotal += pizzaItem.price * cart[i].qt;
+
+            let cartItem = c('.models .cart--item').cloneNode(true);
+
+            let pizzaSizeName;
+            switch (cart[i].size) {
+                case 0:
+                    pizzaSizeName = 'P';
+                    break;
+                case 1:
+                    pizzaSizeName = 'M';
+                    break;
+                case 2:
+                    pizzaSizeName = 'G';
+                    break;
+            }
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                /** se tirar os itens do carrinho apertando o - até  ficar < 0 remove o item do carrinho */
+                if (cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qt++;
+                updateCart();
+            });
+
+            c('.cart').append(cartItem);
+        }
+        /**Calculos do carrinho dentro do for pois eles dependeram dos itens dentro do mesmo */
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+        /**coloca os valores */
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
+    } else {
+        c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
+    }
+}
